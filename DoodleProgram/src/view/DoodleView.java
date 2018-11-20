8dec2bdf1650e3a4a9256c6ba16415df014e5dca
shahboss1 <yash_style93@hotmail.com>
2018-11-19 17:08:57 -0800

    Added rectangle shape. Work in progress.

--- DoodleProgram/src/view/DoodleView.java
+++ DoodleProgram/src/view/DoodleView.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.RetrieveShapes;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,6 +14,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.IShapes;
+import model.Rectangle;
 
 import java.util.ArrayList;
 
@@ -26,21 +28,26 @@ public class DoodleView extends Application
     public static final int MIN_STROKE = 1;
     public static double x1 = 0;
     public static double y1 = 0;
-
     
-
+    //create instance of an object and then call drawshape method on that object
+    public static IShapes shape;
+    
+    public RetrieveShapes controller = new RetrieveShapes();
+    
+    
+    
     //drawing on the canvas
     private Canvas canvas;
-
+    
     //selecting shapes
     private ToggleGroup shapeGroup;
-
+    
     //shape settings
     private ColorPicker fillColorPicker = new ColorPicker();
     private ColorPicker strokeColorPicker = new ColorPicker();
     private Slider strokeSlider;
     private CheckBox filledCheckbox;
-
+    
     @Override
     public void start(Stage stage)
     {
@@ -48,106 +55,106 @@ public class DoodleView extends Application
         stage.setScene(getPrimaryScene());
         stage.show();
     }
-
+    
     private Scene getPrimaryScene()
     {
         BorderPane mainPanel = new BorderPane();
-
+        
         VBox top = new VBox();
         top.getChildren().addAll(buildMenu(), getToolbar());
-
+        
         //set the primary regions
         mainPanel.setTop(top);
         mainPanel.setCenter(getCanvas());
-
+        
         Scene scene = new Scene(mainPanel, WIN_WIDTH, WIN_HEIGHT);
         scene.getStylesheets().add("styles.css");
-
+        
         return scene;
     }
-
+    
     private Parent getToolbar()
     {
         HBox panel = new HBox();
         panel.setId("toolbar-main");
         panel.getChildren().addAll(buildShapeSection(), buildSettings(), buildEdit());
-
+        
         return panel;
     }
-
+    
     private HBox buildShapeSection()
     {
         HBox shapesPanel = new HBox();
         shapesPanel.setId("toolbar-shapes");
-
+        
         String[] shapes = {"Line", "Oval", "Rectangle", "Squiggle"};
         ToggleButton[] buttons = new ToggleButton[shapes.length];
         shapeGroup = new ToggleGroup();
-
+        
         for (int i = 0; i < shapes.length; i++) {
             buttons[i] = getImageToggleButton(shapes[i]);
         }
-
+        
         buttons[0].setSelected(true);
         shapeGroup.getToggles().addAll(buttons);
         shapesPanel.getChildren().addAll(buttons);
-
+        
         return shapesPanel;
     }
-
+    
     private HBox buildSettings()
     {
         HBox settingsPanel = new HBox();
         settingsPanel.setId("toolbar-settings");
-
+        
         styleColorPicker(fillColorPicker);
         styleColorPicker(strokeColorPicker);
-
+        
         VBox strokeBox = new VBox();
         Label strokeLabel = new Label("Stroke: 1");
         strokeSlider = new Slider();
         strokeBox.getChildren().addAll(strokeSlider, strokeLabel);
-
+        
         strokeSlider.setMin(MIN_STROKE);
         strokeSlider.setMax(MAX_STROKE);
         strokeSlider.valueProperty().addListener((observable, oldV, newV) ->
                 strokeLabel.setText("Stroke: " + numToInt(newV)));
-
+        
         filledCheckbox = new CheckBox("Filled");
-
+        
         settingsPanel.getChildren().addAll(new Label("Fill:"), fillColorPicker,
                 new Label("Stroke:"), strokeColorPicker, strokeBox, filledCheckbox);
-
+        
         return settingsPanel;
     }
-
+    
     private void styleColorPicker(ColorPicker picker)
     {
         picker.getStyleClass().add(ColorPicker.STYLE_CLASS_BUTTON);
         picker.setValue(Color.BLACK);
     }
-
+    
     private int numToInt(Number value)
     {
         return (int) Math.floor(value.doubleValue());
     }
-
+    
     private HBox buildEdit()
     {
         HBox editPanel = new HBox();
         editPanel.setId("toolbar-edits");
-
+        
         String[] edits = {"undo", "redo"};
         Button[] buttons = new Button[edits.length];
-
+        
         for (int i = 0; i < edits.length; i++) {
             buttons[i] = getImageButton(edits[i]);
         }
         editPanel.getChildren().addAll(buttons);
-
+        
         return editPanel;
     }
-
+    
     private ImageView getButtonIcon(String text)
     {
         ImageView image = new ImageView(text + ".png");
@@ -155,24 +162,24 @@ public class DoodleView extends Application
         image.setFitWidth(SHAPE_ICON_SIZE);
         return image;
     }
-
+    
     private ToggleButton getImageToggleButton(String text)
     {
         ToggleButton result = new ToggleButton();
         result.setGraphic(getButtonIcon(text));
         return result;
     }
-
+    
     private Button getImageButton(String text)
     {
         Button result = new Button();
         result.setGraphic(getButtonIcon(text));
         return result;
     }
-
+    
     private Parent getCanvas()
     {
-
+        
         VBox box = new VBox();
         
         canvas = new Canvas(WIN_WIDTH, WIN_HEIGHT);
@@ -181,9 +188,10 @@ public class DoodleView extends Application
 //        canvas.widthProperty().bind(box.widthProperty());
 //        canvas.heightProperty().bind(box.heightProperty());
         box.setStyle("-fx-background-color: #FFFFFF;");
+        
         canvas.setOnMousePressed( event->{
-             x1 = event.getX();
-             y1 = event.getY();
+            x1 = event.getX();
+            y1 = event.getY();
 //            double x2 = x1 + 500;
 //            double y2 = y1 + 400;
             graphics.setStroke( Color.rgb( 120,12,43 ) );
@@ -192,29 +200,37 @@ public class DoodleView extends Application
         });
         
         canvas.setOnMouseDragged( event ->{
-        double x2 = event.getX();
-        double y2 = event.getY();
-        graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        graphics.setStroke( Color.rgb( 12,120,43 ) );
-        graphics.setLineWidth( 10 );
-        graphics.strokeLine(x1, y1, x2, y2); //draw a line
-    });
+            double x2 = event.getX();
+            double y2 = event.getY();
+            graphics.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            graphics.setStroke( Color.rgb( 12,120,43 ) );
+            graphics.setLineWidth( 10 );
+            controller.removePreviousShape();
+            controller.addShape( graphics, x1,y1,x2,y2);
+            controller.redrawAllShapes( graphics );
+
+            
+        });
         canvas.setOnMouseReleased( event -> {
             //store to arraylist of shapes
             //set a mouse.clear screen and draw all shapes
-                } );
-    
-        
+            double x2 = event.getX();
+            double y2 = event.getY();
+            controller.addShape( graphics, x1,y1,x2,y2);
+            controller.redrawAllShapes( graphics );
+
+            
+        } );
 
 //        graphics.strokeOval(x, y, width, height); //draw an oval
 //        graphics.strokeRectangle(x, y, width, height); //draw a rectangle
 //        graphics.strokePolyline(x[], y[], numPoints); //draw a several lines connected by points (a squiggle)
-
+        
         box.getChildren().add(canvas);
-
+        
         return box;
     }
-
+    
     private MenuBar buildMenu()
     {
         MenuBar menuBar = new MenuBar();
@@ -222,40 +238,40 @@ public class DoodleView extends Application
         Menu edit = new Menu("Edit");
         Menu draw = new Menu("Draw");
         Menu help = new Menu("Help");
-
+        
         fileMenu(file);
         editMenu(edit);
         drawMenu(draw);
         help(help);
-
+        
         menuBar.getMenus().addAll(file, edit, draw, help);
         return menuBar;
     }
-
+    
     private void fileMenu(Menu file)
     {
         MenuItem[] items = {new MenuItem("Quit")};
         file.getItems().addAll(items);
     }
-
+    
     private void editMenu(Menu edit)
     {
         MenuItem[] items = {new MenuItem("Undo"), new MenuItem("Redo")};
         edit.getItems().addAll(items);
     }
-
+    
     private void drawMenu(Menu draw)
     {
         Menu shapesMenu = new Menu("Shape Tools");
         MenuItem[] shapes = {new MenuItem("Line"), new MenuItem("Oval"),
-                             new MenuItem("Rectangle"), new MenuItem("Squiggle")};
+                new MenuItem("Rectangle"), new MenuItem("Squiggle")};
         shapesMenu.getItems().addAll(shapes);
         draw.getItems().add(shapesMenu);
-
+        
         MenuItem clear = new MenuItem("Clear Shapes");
         draw.getItems().add(clear);
     }
-
+    
     private void help(Menu about)
     {
         MenuItem[] items = {new MenuItem("About")};
